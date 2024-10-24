f61dbbc277e649326be15124d3462b34dd6cbcb0
Jonath117 <rochacontrerasjonathan40@gmail.com>
2024-10-24 16:00:25 -0400

    Imageness web
    puse unas imagenes

--- ejerc_noti/Notis/ClientApp/src/app/home/home.component.ts
+++ ejerc_noti/Notis/ClientApp/src/app/home/home.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   featuredArticles = [
-    { title: 'Artículo 1', description: 'Descripción del artículo 1', image: 'https://via.placeholder.com/150' },
-    { title: 'Artículo 2', description: 'Descripción del artículo 2', image: 'https://via.placeholder.com/150' },
-    { title: 'Artículo 3', description: 'Descripción del artículo 3', image: 'https://via.placeholder.com/150' }
+    { title: 'Artículo 1', description: 'Descripción del artículo 1', image: 'https://img.remediosdigitales.com/4b115d/verstappen-yas-marina-f1-2021/1366_2000.jpeg' },
+    { title: 'Artículo 2', description: 'Descripción del artículo 2', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmy1g_Z0eZTdHI1ggNU6Kmf4CTEIiGUeVw3w&s' },
+    { title: 'Artículo 3', description: 'Descripción del artículo 3', image: 'https://media.revistagq.com/photos/61b7467075b0a617c031aa2e/16:9/w_2560%2Cc_limit/Verstappen%2520bandiera%2520Olanda.jpg' }
   ];
 }
